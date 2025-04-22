130859c5b8bb053f312c7807e8920d4e0efc355e
c0a2419011 <c0a2419011@edu.teu.ac.jp>
2025-04-22 14:05:48 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,13 +4,19 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA={
+    pg.K_UP :(0,-5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0),
+    }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
@@ -23,15 +29,19 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        sum_mv = [0, 0]#合計移動量リスト 
+        for key,mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0] #左右方向
+                sum_mv[1] += mv[1] #上下方向
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
